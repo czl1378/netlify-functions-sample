ce604c6a06f1135cbe65234f5f262c7dca81b73a
czl1378 <czl1378@126.com>
2021-04-05 00:48:12 +0800

    update

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -5,28 +5,46 @@ const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 const storage = new Storage();
 
-exports.handler = async function(event, context) {
-  const { path, body, httpMethod } = event;
-
-  if (
-    !/post/i.test(httpMethod)
-  ) {
-    return {
-      statusCode: 401
-    }
-  }
-
+function error(message) {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello',
-      bucketName: STORAGE_BUCKETNAME,
-      apiKey: GOOGLE_API_KEY,
-      path,
-      body,
-      fileName: body.name,
-      filePath: body.path,
-      bodyType: typeof body
-    })
-  };
+    body: {
+      success: false,
+      message
+    }
+  }
+}
+
+function success(data) {
+  return {
+    statusCode: 200,
+    body: {
+      success: true,
+      data
+    }
+  }
+}
+
+exports.handler = async function(event, context) {
+  const { isBase64Encoded, body, httpMethod, queryStringParameters } = event;
+
+  const { appchainId, isRaw } = queryStringParameters;
+  
+  if (!/post/i.test(httpMethod)) {
+    return error('Method not support');
+  }
+
+  if (!appchainId || !isRaw) {
+    return error('Missing parameters');
+  }
+
+  const myBucket = storage.bucket(STORAGE_BUCKETNAME);
+  const destFileName = `${appchainId}/chain-spec${isRaw == 1 ? '-raw' : ''}.json`;
+
+  const file = myBucket.file(destFileName);
+
+  file.save(Buffer.from(body, isBase64Encoded ? 'base64' : 'utf8'), function(err, res) {
+    return !err ? success(res) : error(err.toString());
+  });
+
 }
